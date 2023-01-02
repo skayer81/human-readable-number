64c2feb55bfcec2ad3c73a793ea0792279a79691
skayer81 <114329023+skayer81@users.noreply.github.com>
2023-01-02 03:27:51 +0300

    fix script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports =
+    function toReadable(number) {
+        const numbers1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+            'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+        const numbers3 = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+        let result = '';
+        let strNumber = String(number);
+        if (strNumber.length === 3) {
+            result += numbers1[Number(strNumber[0])] + ' hundred';
+            if (number % 100 === 0) { return result }
+            result += ' ';
+            strNumber = strNumber.slice(1);
+        }
+        if (strNumber.length === 2) {
+            if (strNumber[0] == 1) { result += numbers1[Number(strNumber)]; return result; }
+            if (strNumber[0] != 0) {
+                result += numbers3[Number(strNumber[0])];
+                if (strNumber[1] == 0) { return result }
+                result += ' ';
+                strNumber = strNumber.slice(1)
+            }
+        }
+        result += numbers1[Number(strNumber)];
+        return result;
+    }
+
